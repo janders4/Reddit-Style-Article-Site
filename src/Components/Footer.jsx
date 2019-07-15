384a23adf58bbdce47d44e8f050f5e67eef6a9ae
janders4 <johnanderson198689@gmail.com>
2019-07-15 12:59:16 +0100

    topics added to nav on hard coded example

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../styles/footer.css";
 
 function Footer(props) {
   return (
     <div className="footer">
-      <h2>Footer</h2>
+      <h2>By John Anderson</h2>
     </div>
   );
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import "../styles/nav.css";
 
 class Nav extends Component {
   render() {
+    const state = {
+      topics: [
+        {
+          slug: "coding",
+          description: "Code is love, code is life"
+        },
+        {
+          slug: "stuff",
+          description: "Code is love, code is life"
+        }
+      ]
+    };
     return (
       <div className="nav">
-        <h2>topics</h2>
+        {state.topics.map(topic => {
+          return <h2>{topic.slug}</h2>;
+        })}
       </div>
     );
   }
+  componentDidMount() {}
 }
 
 export default Nav;
